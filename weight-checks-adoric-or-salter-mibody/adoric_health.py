09bce2f97e812b3e5e88978898878a3240ee8e28
Tomasz - DE <infotip@infotip.co.uk>
2025-08-03 18:33:51 +0100

    kombinowanie ze sciezka dostepu

--- weight-checks-adoric-or-salter-mibody/adoric_health.py
+++ weight-checks-adoric-or-salter-mibody/adoric_health.py
@@ -16,16 +16,16 @@ import random
 # full_path = './Data'
 # full_path = 
 # '/mount/src/my_projects/weight-checks-adoric-or-salter-mibody/Data'
-full_path = '.streamlit/weight-checks-adoric-or-salter-mibody/Data'
+full_path = '.streamlit/my_projects/weight-checks-adoric-or-salter-mibody/Data'
 
-'''
-# pick current dir - not needed for this purpose
-# and change working dir to the required one
-# current_dir = os.getcwd()
-# os.chdir(full_path)
-# xxxxxx_st.write(os.getcwd)
-# print(full_path)
-# '''
+## '''
+## pick current dir - not needed for this purpose
+## and change working dir to the required one
+## current_dir = os.getcwd()
+### os.chdir(full_path)
+## xxxxxx_st.write(os.getcwd)
+## print(full_path)
+## '''
 
 data_line_by_line, \
     numer_of_files, \
