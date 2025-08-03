fb0709cbb24d81df070221e99f61381a9f19f2a0
Tomasz - DE <infotip@infotip.co.uk>
2025-08-03 19:07:33 +0100

    proby ruszenie w chmurze 002 dziala

--- weight-checks-adoric-or-salter-mibody/adoric_health.py
+++ weight-checks-adoric-or-salter-mibody/adoric_health.py
@@ -16,19 +16,23 @@ import random
 # full_path = './Data'
 # full_path = 
 # '/mount/src/my_projects/weight-checks-adoric-or-salter-mibody/Data'
-full_path = '/mount/src/my_projects/weight-checks-adoric-or-salter-mibody/Data'
+# full_path = '/mount/src/my_projects/weight-checks-adoric-or-salter-mibody/Data'
 
-st.write(os.environ.items())
+# ! bardzo przydatna linia!!!
+# ! st.write(os.environ.items())
+# ! ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑
 
-# if os.environ.get("IS_STREAMLIT_SHARING"):
-#     full_path = \
-#         '/mount/src/my_projects/weight-checks-adoric-or-salter-mibody/Data'
-# else:
-#     drive = 'u:'
-#     path_a = 'OneDrive'
-#     path_b = 'DRIVE_GOOGLE'
-#     path_c = 'Adoric health'
-#     full_path = os.path.join(drive, '/', path_a, path_b, path_c)
+st.write(os.environ['HOSTNAME'])
+
+if os.environ.get('HOSTNAME') == 'streamlit':
+    full_path = \
+        os.getcwd + '/weight-checks-adoric-or-salter-mibody/Data'
+else:
+    drive = 'u:'
+    path_a = 'OneDrive'
+    path_b = 'DRIVE_GOOGLE'
+    path_c = 'Adoric health'
+    full_path = os.path.join(drive, '/', path_a, path_b, path_c)
 
 ## '''
 ## pick current dir - not needed for this purpose
