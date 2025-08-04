91e802afa0f1b70e4bcd9e8f544457ecf86396db
Tomasz - DE <infotip@infotip.co.uk>
2025-08-04 07:30:32 +0100

    random pokemons displayed

--- weight-checks-adoric-or-salter-mibody/adoric_health.py
+++ weight-checks-adoric-or-salter-mibody/adoric_health.py
@@ -352,4 +352,4 @@ for i in range(4):
                 f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{pokedex_number}.png'
             )
 
-st.write(len(pokemon_df['name'].to_list()),len(pokemon_df['name'].unique().to_list()))
+st.write(len(pokemon_df['name']),len(pokemon_df['name'].unique()))
